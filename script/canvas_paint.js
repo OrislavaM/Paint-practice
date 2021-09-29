4d69b1bf120785fdacf96d03b7da1d6f3d3878e1
OrislavaM <orisyamatvienko@gmail.com>
2021-09-30 01:31:53 +0300

    new structure html, styles css, events in js

--- script/canvas_paint.js
+++ script/canvas_paint.js
@@ -1,6 +1,16 @@
+// меняем толщину линии,
+// ее цвет,
+// цвет фона,
+// добавляем возможность undo на несколько шагов назад.
+// сохраняем рисунок в файл на диск девайса.
+// удаление резинкой
+// ______________________________________________________
+
 const canvas = document.getElementById("canvas");
 
 const ctx = canvas.getContext("2d");
+
+const clear = document.getElementById("clear");
 
 let thickness;
 document.getElementById("thickness").addEventListener("change", (e) => {
@@ -14,6 +24,11 @@ const drawingFn = function (moveEvent) {
     ctx.lineJoin = "round";
     ctx.stroke();
 };
+
+clear.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
 canvas.addEventListener("mousedown", (e) => {
     ctx.beginPath();
     let x = e.offsetX;
